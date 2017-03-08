47fca0dab3d19704fde3d7c89a36861958ebd13b
bloomin711 <email@stevencbloom.com>
2017-03-08 12:15:44 -0500

    CodeMirror

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+var mammoth = require("mammoth");
+
+mammoth.convertToHtml({ path:"path/to/document.docx" })
+    .then(function(results) {
+        "use strict";
+        var html = result.value; // The generated HTML
+        var messages = result.messages; // Any messages, such as warnings during conversion
+
+        console.log(html); 
+        console.log(messages);
+    })
+    .done();
